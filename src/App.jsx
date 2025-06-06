81f3faa5dc520e584f99f36448de30d99824abf1
Michael Ager Midenga <agermidenga@gmail.com>
2025-06-06 15:19:18 +0300

    my second react project creating a youtube clone

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Video from './Pages/Video/Video';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './Sidebar.css';
+import home from '../../assets/home.png';
+import game_icon from '../../assets/game_icon.png';
+import automobiles from '../../assets/automobiles.png';
+import music from '../../assets/music.png';
+import news from '../../assets/news.png';   
+import tech from '../../assets/tech.png';
+import sports from '../../assets/sports.png';
+import entertainment from '../../assets/entertainment.png';
+import blogs from '../../assets/blogs.png';
+import jack from '../../assets/jack.png';
+import simon from '../../assets/simon.png';
+import megan from '../../assets/megan.png';
+import cameron from '../../assets/cameron.png';
+import tom from '../../assets/tom.png';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="shortcut-links">
+            <div className="side-links">
+                <img src={home} alt="" /><p>Home</p>
+            </div>
+            <div className="side-links">
+                <img src={game_icon} alt="" /><p>Gaming</p>
+            </div>
+            <div className="side-links">
+                <img src={automobiles} alt="" /><p>Automobiles</p>
+            </div>
+            <div className="side-links">
+                <img src={music} alt="" /><p>Music</p>
+            </div>
+            <div className="side-links">
+                <img src={news} alt="" /><p>News</p>
+            </div>
+            <div className="side-links">
+                <img src={tech} alt="" /><p>Tech</p>
+            </div>
+            <div className="side-links">
+                <img src={sports} alt="" /><p>Sports</p>
+            </div>
+            <div className="side-links">
+                <img src={entertainment} alt="" /><p>Entertainment</p>
+            </div>
+            <div className="side-links">
+                <img src={blogs} alt="" /><p>Blogs</p>
+            </div>
+            <div className="side-links">
+                <img src={jack} alt="" /><p>Jack</p>
+            </div>
+            <div className="side-links">
+                <img src={simon} alt="" /><p>Simon</p>
+            </div>
+            <div className="side-links">
+                <img src={megan} alt="" /><p>Megan</p>
+            </div>
+            <div className="side-links">
+                <img src={cameron} alt="" /><p>Cameron</p>
+            </div>
+        </div>
+        <div className="side-links">
+                <img src={tom} alt="" /><p>Tom</p>
+            </div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
