d875809ce452def17948bc0c39747bd45e4c35a7
Michael Ager Midenga <agermidenga@gmail.com>
2025-06-14 12:41:32 +0300

    set the recommended list videos

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PlayVideo from '../../Components/Play Video/PlayVideo'
-
+import './video.css';
+import Recommended from '../../Components/Recommended/Recommended';
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo/>
+      <Recommended/>
       
     </div>
   )
